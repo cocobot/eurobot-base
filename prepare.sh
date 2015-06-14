f78126ef0fb85ffbebbe877dc50f470354ffb3ff
Damien Espitallier <damien.espitallier@gmail.com>
2015-06-14 09:32:00 +0200

    autodownload other repositories

---
+++ prepare.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+#getting hardware
+git clone git@github.com:cocobot/hardware.git
+
+
+#getting aussbee fork
+git clone git@github.com:cocobot/ausbee.git
+
+
+#getting ausbee custom platforms
