6bb15004e5c28cc30dc579a97f8cd1125a5671de
Damien Espitallier <damien.espitallier@gmail.com>
2015-06-17 22:37:01 +0200

    use MCUAL for GPIO

--- brain_2015/platform.c
+++ brain_2015/platform.c
@@ -1,48 +1,65 @@
-#include <stm32f4xx.h>
+#include <mcual.h>
 #include "platform.h"
 
-#define PLATFORM_PLLM 8
-#define PLATFORM_PLLN 336
-#define PLATFORM_PLLP 2
-#define PLATFORM_PLLQ 7
+#define PLATFORM_MAIN_CLOCK_KHZ 180000
 
 
 void platform_init(void)
 {
-  //init Quartz and PLL
-  RCC_DeInit();
-
-  RCC_HSEConfig(RCC_HSE_ON);
-  RCC_WaitForHSEStartUp();
-
-  RCC_PLLConfig(RCC_PLLSource_HSE, PLATFORM_PLLM, PLATFORM_PLLN, PLATFORM_PLLP, PLATFORM_PLLQ);
-
-  RCC_PLLCmd(ENABLE);
-
-  while (!(RCC->CR & RCC_CR_PLLRDY))
-    ;
-
-  RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+  //init clock
+  mcual_clock_init(MCUAL_CLOCK_SOURCE_EXTERNAL, PLATFORM_MAIN_CLOCK_KHZ); 
 
   
   //init leds
+  mcual_gpio_init(MCUAL_GPIOG, MCUAL_GPIO_PIN13, MCUAL_GPIO_OUTPUT);
+  mcual_gpio_init(MCUAL_GPIOG, MCUAL_GPIO_PIN14, MCUAL_GPIO_OUTPUT);
+  mcual_gpio_clear(MCUAL_GPIOG, MCUAL_GPIO_PIN13);
+  mcual_gpio_clear(MCUAL_GPIOG, MCUAL_GPIO_PIN14);
+  /*
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
   GPIO_InitTypeDef GPIOInitStruct_G;
   GPIOInitStruct_G.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
   GPIOInitStruct_G.GPIO_Speed = GPIO_Speed_50MHz;
   GPIOInitStruct_G.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_Init(GPIOG, &GPIOInitStruct_G);
+  */
 }
 
 void platform_led_toggle(uint8_t led)
 {
   if(led & PLATFORM_LED0)
   {
-    GPIO_ToggleBits(GPIOG, GPIO_Pin_13);
+    mcual_gpio_toogle(MCUAL_GPIOG, MCUAL_GPIO_PIN13);
   }
 
   if(led & PLATFORM_LED1)
   {
-    GPIO_ToggleBits(GPIOG, GPIO_Pin_14);
+    mcual_gpio_toogle(MCUAL_GPIOG, MCUAL_GPIO_PIN14);
+  }
+}
+
+void platform_led_set(uint8_t led)
+{
+  if(led & PLATFORM_LED0)
+  {
+    mcual_gpio_set(MCUAL_GPIOG, MCUAL_GPIO_PIN13);
+  }
+
+  if(led & PLATFORM_LED1)
+  {
+    mcual_gpio_set(MCUAL_GPIOG, MCUAL_GPIO_PIN14);
+  }
+}
+
+void platform_led_clear(uint8_t led)
+{
+  if(led & PLATFORM_LED0)
+  {
+    mcual_gpio_clear(MCUAL_GPIOG, MCUAL_GPIO_PIN13);
+  }
+
+  if(led & PLATFORM_LED1)
+  {
+    mcual_gpio_clear(MCUAL_GPIOG, MCUAL_GPIO_PIN14);
   }
 }
--- brain_2015/platform.h
+++ brain_2015/platform.h
@@ -2,13 +2,13 @@
 #define PLATFORM_H
 
 #include <stdint.h>
-#include <stm32f4xx_gpio.h>
-#include <stm32f4xx_rcc.h>
 
 #define PLATFORM_LED0 (1 << 0)
 #define PLATFORM_LED1 (1 << 1)
 
 void platform_init(void);
 void platform_led_toggle(uint8_t led);
+void platform_led_set(uint8_t led);
+void platform_led_clear(uint8_t led);
 
 #endif// PLATFORM_H
