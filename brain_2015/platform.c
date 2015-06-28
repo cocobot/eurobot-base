5055d18976a236521a96593c6f6b955bc3d445a5
Damien Espitallier <damien.espitallier@gmail.com>
2015-06-28 17:15:23 +0200

    add new Konfig

--- brain_2015/platform.c
+++ brain_2015/platform.c
@@ -8,21 +8,18 @@ void platform_init(void)
 {
   //init clock
   mcual_clock_init(MCUAL_CLOCK_SOURCE_EXTERNAL, PLATFORM_MAIN_CLOCK_KHZ); 
-
   
   //init leds
   mcual_gpio_init(MCUAL_GPIOG, MCUAL_GPIO_PIN13, MCUAL_GPIO_OUTPUT);
   mcual_gpio_init(MCUAL_GPIOG, MCUAL_GPIO_PIN14, MCUAL_GPIO_OUTPUT);
   mcual_gpio_clear(MCUAL_GPIOG, MCUAL_GPIO_PIN13);
   mcual_gpio_clear(MCUAL_GPIOG, MCUAL_GPIO_PIN14);
-  /*
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
-  GPIO_InitTypeDef GPIOInitStruct_G;
-  GPIOInitStruct_G.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
-  GPIOInitStruct_G.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIOInitStruct_G.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_Init(GPIOG, &GPIOInitStruct_G);
-  */
+  
+  //init uart1 pin
+  mcual_gpio_init(MCUAL_GPIOA, MCUAL_GPIO_PIN9, MCUAL_GPIO_OUTPUT);
+  mcual_gpio_init(MCUAL_GPIOB, MCUAL_GPIO_PIN7, MCUAL_GPIO_INPUT);
+  mcual_gpio_set_function(MCUAL_GPIOA, MCUAL_GPIO_PIN9, 7);
+  mcual_gpio_set_function(MCUAL_GPIOB, MCUAL_GPIO_PIN7, 7);
 }
 
 void platform_led_toggle(uint8_t led)
