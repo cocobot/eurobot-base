da824e1fc483fab9134ab5cc6087bf7d01845159
Damien Espitallier <damien.espitallier@gmail.com>
2015-06-30 08:31:27 +0200

    new mk/Kconfig for custom packages

--- ausbee-packages/libcocobot/package.mk
+++ ausbee-packages/libcocobot/package.mk
@@ -1,27 +1,20 @@
 LIBCOCOBOT_VERSION = 0.1
-LIBCOCOBOT_URL = https://github.com/cocobot/ausbee-libcocobot/archive/$(LIBCOCOBOT_VERSION).tar.gz
+LIBCOCOBOT_URL = https://github.com/cocobot/libcocobot/archive/$(LIBCOCOBOT_VERSION).tar.gz
 LIBCOCOBOT_ARCHIVE = libcocobot-$(LIBCOCOBOT_VERSION).tar.gz
+LIBCOCOBOT_GIT_UPSTREAM_URL = git@github.com:cocobot/libcocobot.git
 
+LIBCOCOBOT_DEPENDENCIES=mcual operating_systems
+
+# File path
+ifeq ($(CONFIG_LIBCOCOBOT_USE_GIT),y)
+LIBCOCOBOT_FILE_PATH=git
+else
+LIBCOCOBOT_FILE_PATH=mcual-$(LIBCOCOBOT_VERSION)
+endif
 
 # Include files
-LIBCOCOBOT_INCLUDE_PATH=libcocobot-$(LIBCOCOBOT_VERSION)/include/
+LIBCOCOBOT_INCLUDE_PATH=$(LIBCOCOBOT_FILE_PATH)/include/
 
-LIBCOCOBOT_SRC_FILES=
-
-#ifeq ($(CONFIG_LIBCOCOBOT_DRIVERS),y)
-#LIBCOCOBOT_SRC_FILES=libcocobot-$(LIBCOCOBOT_VERSION)/src/drivers/lidar.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/drivers/ax12.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/drivers/encoder.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/drivers/l298_driver.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/drivers/servo.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/drivers/gp2.c
-#endif
-#
-#ifeq ($(CONFIG_LIBCOCOBOT_CONTROL_SYSTEM),y)
-#LIBCOCOBOT_SRC_FILES+=libcocobot-$(LIBCOCOBOT_VERSION)/src/control_system/control_system_manager.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/control_system/controller/pid.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/control_system/filters/diff.c \
-#	libcocobot-$(LIBCOCOBOT_VERSION)/src/control_system/filters/quadramp.c
-#endif
+LIBCOCOBOT_SRC_FILES=$(LIBCOCOBOT_FILE_PATH)/src/console/console.c
 
 $(eval $(call pkg-generic,LIBCOCOBOT))
