ced2167e8d75833eafcd6128ce22a8ec87fc1631
Damien Espitallier <damien.espitallier@gmail.com>
2015-10-21 13:58:17 +0200

    use new ausbee package-generic

--- brain_2016/platform.mk
+++ brain_2016/platform.mk
@@ -1,17 +1,6 @@
-# Set the source path of this platform
-PLATFORM_BRAIN_2016_PATH=$(CONFIG_CUSTOM_PLATFORMS_PATH)/brain_2016
 
-# Includes of this platform
-PLATFORM_INCLUDES=-I"$(PLATFORM_BRAIN_2016_PATH)"
+BRAIN_2016_LOCAL_FILE_PATH=$(CONFIG_CUSTOM_PLATFORMS_PATH)/brain_2016
+BRAIN_2016_LOCAL_SRC_FILES=platform.c
+BRAIN_2016_LOCAL_INCLUDE_PATH=.
 
-# Search the source file and generate the object list
-PLATFORM_SRC_FILES+=$(PLATFORM_BRAIN_2016_PATH)/platform.c
-PLATFORM_OBJ_FILES=$(patsubst ${CONFIG_CUSTOM_PLATFORMS_PATH}/%.c,${OUTPUT_PATH}/%.o,${PLATFORM_SRC_FILES})
-
-#Add the object file list to the global object list
-OBJ_FILES+=$(PLATFORM_OBJ_FILES)
-
-$(PLATFORM_OBJ_FILES): $(OUTPUT_PATH)/%.o :$(CONFIG_CUSTOM_PLATFORMS_PATH)/%.c $(TOOLCHAIN_EXTRACTED) MCUAL_LOAD
-	@mkdir -p $(dir $@)
-	$(HOST_CC) -o $@ $(HOST_CFLAGS) $(MCUAL_INCLUDES) $(SYSTEM_INCLUDES) $(HOST_OPTIMISATION) -c $<
-
+$(eval $(call pkg-generic,BRAIN_2016))
