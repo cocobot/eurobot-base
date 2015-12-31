0e342d662517d4596cb6acf1f6486b6bbf678570
David Bitonneau <david.bitonneau@gmail.com>
2015-12-31 18:28:05 +0100

    Connection to vrep removed from mcual

--- src/sim/clock.c
+++ src/sim/clock.c
@@ -1,5 +1,4 @@
 #include <mcual.h>
-#include "extApi.h"
 
 
 
@@ -9,21 +8,6 @@ void mcual_clock_init(mcual_clock_source_t source, int32_t target_freq_kHz)
   (void)target_freq_kHz;
 
   //create thread for simulated peripherals
-  mcual_arch_sim_init_peripherals();
-
-
-  int clientID = simxStart((simxChar*)"127.0.0.1", 1234, 1, 1, 2000, 5);
-	if (clientID!=-1)
-  {
-    printf("Oh yeah !\n");
-    while (simxGetConnectionId(clientID)!=-1)
-    {
-
-      extApi_sleepMs(5);
-    }
-  }
-  else
-  {
-    printf("Echec de la connexion a VREP\n");
-  }
+  // TODO: is this line really needed?
+  //mcual_arch_sim_init_peripherals();
 }
