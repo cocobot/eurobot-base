07219279e21b26af3103bdee4dc2ecda6e1e923c
David Bitonneau <david.bitonneau@gmail.com>
2016-01-02 13:25:21 +0100

    Put back super important line stupidly commented previously

--- src/sim/clock.c
+++ src/sim/clock.c
@@ -8,6 +8,5 @@ void mcual_clock_init(mcual_clock_source_t source, int32_t target_freq_kHz)
   (void)target_freq_kHz;
 
   //create thread for simulated peripherals
-  // TODO: is this line really needed?
-  //mcual_arch_sim_init_peripherals();
+  mcual_arch_sim_init_peripherals();
 }
