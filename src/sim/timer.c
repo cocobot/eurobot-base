e92271fa741d77dd35684c88d3b90e449267e8a3
Damien Espitallier <damien.espitallier@gmail.com>
2016-02-07 10:44:54 +0100

    add stubs for timer simulation

---
+++ src/sim/timer.c
@@ -0,0 +1,20 @@
+#include <mcual.h>
+
+void mcual_timer_init(mcual_timer_t timer, uint32_t freq_Hz)
+{
+  (void)timer;
+  (void)freq_Hz;
+}
+
+void mcual_timer_enable_channel(mcual_timer_t timer, mcual_timer_channel_t channel)
+{
+  (void)timer;
+  (void)channel;
+}
+
+void mcual_timer_set_duty_cycle(mcual_timer_t timer, mcual_timer_channel_t channel, uint32_t duty_cycle)
+{
+  (void)timer;
+  (void)channel;
+  (void)duty_cycle;
+}
