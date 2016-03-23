070a4b20fd6a9df73f14763597523dd011f41b11
Damien Espitallier <damien.espitallier@gmail.com>
2016-03-23 21:13:19 +0100

    handle mcual loader

--- cocoui.js
+++ cocoui.js
@@ -6,15 +6,21 @@ var http = require('http');
 var bodyParser = require('body-parser');
 var optimist = require('optimist');
 var path = require('path');
-var protocol = require('./protocol/protocol.js');
 var jsxCompile = require('express-jsx');
+var multer  = require('multer');
+
+var protocol = require('./protocol/protocol.js');
+var hexParser = require('./hexparser.js');
+
 
 //set defaults
 optimist = optimist.default('http', 3000);
 var argv = optimist.argv;
 
 //create express app
-var app = express()
+var app = express();
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
 app.use(jsxCompile(path.join(__dirname, 'public'), {
     dest: path.join(__dirname, 'compiled-js')
 }));
@@ -52,6 +58,56 @@ app.get('/api/time/:ct', function(req, res) {
     'diff': diff,
     'serverTimestamp': serverTimestamp,
   });
+});
+app.post('/api/program', upload.single('file'), function(req, res) {
+  var sendMsg = function(msg) {
+    res.write(msg + "\n");
+    io.sockets.emit("bootloader", {msg: msg});
+  };
+
+  var sendEnd = function(success) {
+    if(success) {
+      res.write("Done\n");
+      io.sockets.emit("bootloader", {msg: "Done"});
+    }
+    else {
+      res.write("Fail\n");
+      io.sockets.emit("bootloader", {msg: "Fail"});
+    }
+    res.end();
+  };
+  
+  sendMsg("Parsing hex file");
+  var parser = hexParser.parse(req.file, function(success, msg, param) {
+    if(!success) {
+      sendMsg(msg);
+    }
+    else {
+      protocol.program(param, sendMsg, function(success, msg) {
+        if(!success) {
+          sendMsg(msg);
+          sendEnd(false);
+        }
+        else {
+          sendEnd(true);
+        }
+      });
+    }
+  });
+
+
+  //var f = function(res, i, cb) {
+  //  console.log("pouet");
+  //  if(i < 5) {
+  //    res.write("plop " + i + "\n")
+  //    setTimeout(function() { cb(res, i + 1, cb);} , 1000);
+  //  }
+  //  else {
+  //    res.write("plop " + i + "\n")
+  //  }
+  //}
+
+  //setTimeout(function() { f(res, 0, f);} , 1000);
 });
 
 //start serveur
