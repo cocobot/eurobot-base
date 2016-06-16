78087b96e8101e7e5863d46eafe6a5b06b56be43
David Bitonneau <david.bitonneau@gmail.com>
2016-06-16 12:39:58 +0200

    Fixed other little copy/paster issue in global package.mk

--- ausbee-packages/packages.mk
+++ ausbee-packages/packages.mk
@@ -6,6 +6,6 @@ ifeq ($(CONFIG_MCUAL),y)
 include $(CONFIG_CUSTOM_PACKAGES_PATH)/mcual/package.mk
 endif
 
-ifeq ($(CONFIG_MCUAL),y)
+ifeq ($(CONFIG_VREP),y)
 include $(CONFIG_CUSTOM_PACKAGES_PATH)/vrep/package.mk
 endif
