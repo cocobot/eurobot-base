0501bd027b8ea065bd43012c3a766e5b2962a45d
Damien Espitallier <damien.espitallier@gmail.com>
2016-06-26 11:56:20 +0200

    Add a way to auto-discover package source files. Useful if you are working on a separated branch with a different source file list

--- ausbee-packages/libcocobot/package.mk
+++ ausbee-packages/libcocobot/package.mk
@@ -13,24 +13,16 @@ endif
 # Include files
 LIBCOCOBOT_INCLUDE_PATH=$(LIBCOCOBOT_FILE_PATH)/include/
 
-LIBCOCOBOT_SRC_FILES= $(LIBCOCOBOT_FILE_PATH)/src/console/console.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/lcd/lcd.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/trajectory/trajectory.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/asserv/asserv.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/asserv/ramp.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/asserv/pid.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/action_scheduler/action_scheduler.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/position/position.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/game_state/game_state.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/opponent_detection/opponent_detection.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/pathfinder/pathfinder.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/pathfinder/pathfinder_internal.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/pathfinder/pathfinder_table.c
-LIBCOCOBOT_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/pathfinder/pathfinder_douglas_peucker.c
+#find all .c files and populate LIBCOCOBOT_SRC_FILES
+$(eval $(call pkg-generic-auto-discover-common-srcs,LIBCOCOBOT))
 
 LIBCOCOBOT_TARGET_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/position/encoders.c
-
 LIBCOCOBOT_SIM_SRC_FILES+=$(LIBCOCOBOT_FILE_PATH)/src/position/vrep.c
+
+#remove target and sim sources files to autodiscovered list
+LIBCOCOBOT_SRC_FILES:=$(filter-out $(LIBCOCOBOT_SIM_SRC_FILES),$(LIBCOCOBOT_SRC_FILES))
+LIBCOCOBOT_SRC_FILES:=$(filter-out $(LIBCOCOBOT_TARGET_SRC_FILES),$(LIBCOCOBOT_SRC_FILES))
+
 
 GLOBAL_LDFLAGS+=-lm
 
