0643e9e8e190d39ae8aca6d160b267c74a986eed
vfaure <vincent.hr.faure@gmail.com>
2016-08-15 17:06:28 +0200

    add platform nucleo_401

--- platforms.mk
+++ platforms.mk
@@ -8,6 +8,11 @@ include $(CONFIG_CUSTOM_PLATFORMS_PATH)/brain_2016/platform.mk
 PLATFORM_CLEAN_GOAL+=platform_brain_2016-clean
 endif
 
+ifeq ($(CONFIG_PLATFORM_NUCLEO_401),y)
+include $(CONFIG_CUSTOM_PLATFORMS_PATH)/nucleo_401/platform.mk
+PLATFORM_CLEAN_GOAL+=platform_nucleo_401-clean
+endif
+
 ifeq ($(CONFIG_PLATFORM_PRISM_V1),y)
 include $(CONFIG_CUSTOM_PLATFORMS_PATH)/prism_v1/platform.mk
 PLATFORM_CLEAN_GOAL+=prism_v1-clean
