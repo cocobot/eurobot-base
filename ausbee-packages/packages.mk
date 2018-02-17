b126e09a52a20bf3e0fc29baf63d6216c87065ca
Damien Espitallier <damien.espitallier@gmail.com>
2018-02-17 20:00:29 +0100

    add ST api files

--- ausbee-packages/packages.mk
+++ ausbee-packages/packages.mk
@@ -6,6 +6,10 @@ ifeq ($(CONFIG_MCUAL),y)
 include $(CONFIG_CUSTOM_PACKAGES_PATH)/mcual/package.mk
 endif
 
+ifeq ($(CONFIG_VL53L0X),y)
+include $(CONFIG_CUSTOM_PACKAGES_PATH)/vl53l0x/package.mk
+endif
+
 ifeq ($(CONFIG_VREP),y)
 include $(CONFIG_CUSTOM_PACKAGES_PATH)/vrep/package.mk
 endif
--- ausbee-packages/vl53l0x/package.mk
+++ ausbee-packages/vl53l0x/package.mk
@@ -6,8 +6,4 @@ VL53L0X_LOCAL_INCLUDE_PATH=include/
 #find all .c files and populate VL53L0X_SRC_FILES
 $(eval $(call pkg-generic-auto-discover-local-common-srcs,VL53L0X))
 
-#remove target and sim sources files to autodiscovered list
-VL53L0X_LOCAL_SRC_FILES:=$(filter-out $(VL53L0X_TEMP_SIM_FILES),$(VL53L0X_LOCAL_SRC_FILES))
-VL53L0X_LOCAL_SRC_FILES:=$(filter-out $(VL53L0X_TARGET_LOCAL_SRC_FILES),$(VL53L0X_LOCAL_SRC_FILES))
-
 $(eval $(call pkg-generic,VL53L0X))
