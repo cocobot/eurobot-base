95f1118b9137de0048cc137d39f50b2b2affe164
vfaure <vincent.hr.faure@gmail.com>
2018-03-04 14:01:06 +0100

    add pbrain 2018

---
+++ projects/pbrain_2018/cocobot_pathfinder_config.c
@@ -0,0 +1,9 @@
+#include "cocobot_pathfinder_config.h"
+#include "cocobot.h"
+
+
+cocobot_pathfinder_table_init_s initTable [] = 
+{
+    {},
+    {0, 0, 0, 0, 0, 0}//Last line
+};
---
+++ projects/pbrain_2018/cocobot_pathfinder_config.h
@@ -0,0 +1,20 @@
+#ifndef PATHFINDER_CONFIG_H
+#define PATHFINDER_CONFIG_H
+
+//Table dimension (mm)
+#define TABLE_LENGTH 3000
+#define TABLE_WIDTH 2000
+
+//Opponent half diag dimension
+#define OPPONENT_HALF_DIAG 225
+
+//Grid size
+#define GRID_SIZE 50
+
+//Robot dimensions (mm)
+#define ROBOT_LENGTH    300
+#define ROBOT_WIDTH     300
+
+#define ROBOT_HALF_WIDTH
+
+#endif
