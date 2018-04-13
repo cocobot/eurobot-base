1866240aaab129e58efbb11c3f2dc5d4b3e3d6f8
Damien Espitallier <damien.espitallier@gmail.com>
2018-04-13 23:33:59 +0200

    add blinking led for testing freeRTOS/ausbee/mcual on Xmega

--- projects/step/main.c
+++ projects/step/main.c
@@ -5,10 +5,23 @@
 #include <task.h>
 #include <mcual.h>
 
+void update_led(void * arg)
+{
+  (void)arg;
+
+  while(1)
+  {
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    platform_led_toggle(PLATFORM_LED0);
+  }
+}
 
 int main(void) 
 {
   platform_init();
+
+  xTaskCreate(update_led, "blink", 200, NULL, 1, NULL);
+
   vTaskStartScheduler();
 
   return 0;
