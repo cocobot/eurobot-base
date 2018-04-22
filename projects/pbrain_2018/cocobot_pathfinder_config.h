e3e674ec6928eadfe24d0b432c39679355c35061
vfaure <vincent.hr.faure@gmail.com>
2018-04-22 15:27:09 +0200

    End pathfinder table config for pbrain

--- projects/pbrain_2018/cocobot_pathfinder_config.h
+++ projects/pbrain_2018/cocobot_pathfinder_config.h
@@ -2,22 +2,22 @@
 #define PATHFINDER_CONFIG_H
 
 //Table dimension (mm)
-#define TABLE_LENGTH 3000
-#define TABLE_WIDTH 2000
+#define TABLE_LENGTH            3000
+#define TABLE_WIDTH             2000
 
 //Robot dimensions (mm)
-#define ROBOT_LENGTH    300
-#define ROBOT_WIDTH     300
+#define ROBOT_LENGTH            300
+#define ROBOT_WIDTH             300
 
 //Opponent half diag dimension
-#define OPPONENT_HALF_DIAG 225
+#define OPPONENT_HALF_DIAG      225
 
 //Grid size
-#define GRID_SIZE 50
+#define GRID_SIZE               50
 
 //Table dimension (grid type)
-#define TABLE_LENGTH_GRID   60
-#define TABLE_WIDTH_GRID    40
+#define TABLE_LENGTH_GRID       60
+#define TABLE_WIDTH_GRID        40
 
 // (ROBOT_WIDTH / GRID_SIZE) / 2
 // rounded to superior
