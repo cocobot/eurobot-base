9201e3db55ff0c1b0a42d394ba12c2c53b6ad41f
vfaure <vincent.hr.faure@gmail.com>
2018-05-08 19:08:34 +0200

    Change prio of com handler in pbrain + some stuff in main ...

--- projects/pbrain_2018/main.c
+++ projects/pbrain_2018/main.c
@@ -4,6 +4,7 @@
 #include <task.h>
 #include <mcual.h>
 #include <cocobot.h>
+#include "cocobot/encoders.h"
 //#include "cocobot_pathfinder_config.h"
 
 //static unsigned int _shell_configuration;
@@ -32,6 +33,13 @@ void update_lcd(void * arg)
   }
 #endif
 
+#ifdef AUSBEE_SIM
+  while(1)
+  {
+      cocobot_com_printf(".");
+      vTaskDelay(2000/portTICK_PERIOD_MS);
+  }
+#endif
   //blink for the fun
   int i;
   for(i = 0; i < 20; i += 1)
@@ -82,8 +90,56 @@ void run_strategy(void * arg)
   (void)arg;
 
   cocobot_game_state_wait_for_starter_removed();
+  //int32_t motEnc[2] = {0};
 
+  //cocobot_asserv_set_state(COCOBOT_ASSERV_DISABLE);
+  //cocobot_trajectory_goto_d(100, COCOBOT_TRAJECTORY_UNLIMITED_TIME);
+  //while(1)
+  //{
+  //    cocobot_encoders_get_motor_position(motEnc);
+  //    cocobot_com_printf("Enc 0: %d, Enc 1: %d", motEnc[0], motEnc[1]);
+  //    vTaskDelay(500/portTICK_PERIOD_MS);
+  //}
 
+  //while(1)
+  //{
+  //    cocobot_trajectory_goto_a(90, 10000);
+  //    cocobot_trajectory_goto_a(180, 10000);
+  //    cocobot_trajectory_goto_a(-90, 10000);
+  //    cocobot_trajectory_goto_a(0, 10000);
+  //    cocobot_trajectory_wait();
+  //    cocobot_trajectory_goto_a(-90, 10000);
+  //    cocobot_trajectory_goto_a(180, 10000);
+  //    cocobot_trajectory_goto_a(90, 10000);
+  //    cocobot_trajectory_goto_a(0, 10000);
+  //    cocobot_trajectory_wait();
+  //}
+
+  //cocobot_trajectory_goto_d(1200, 7000);
+
+  //while(1)
+  //{
+      cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), 1000, -600, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
+      cocobot_trajectory_wait();
+     // cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), 1000, 600, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
+      //cocobot_trajectory_wait();
+  //}
+  while(0)
+  {
+      cocobot_trajectory_goto_d(200, 5000);
+      cocobot_trajectory_goto_a(90, 10000);
+      cocobot_trajectory_wait();
+      cocobot_trajectory_goto_d(200, 5000);
+      cocobot_trajectory_goto_a(180, 10000);
+      cocobot_trajectory_wait();
+      cocobot_trajectory_goto_d(200, 5000);
+      cocobot_trajectory_goto_a(-90, 10000);
+      cocobot_trajectory_wait();
+      cocobot_trajectory_goto_d(200, 5000);
+      cocobot_trajectory_goto_a(0, 10000);
+      cocobot_trajectory_wait();
+  }
+  //while(1);
   //while(0)
   //{
   //  cocobot_trajectory_goto_d(-5000, -1);
@@ -92,25 +148,25 @@ void run_strategy(void * arg)
   //  vTaskDelay(5000 / portTICK_PERIOD_MS);
   //}
 
-  cocobot_pathfinder_set_start_zone_allowed();
-  cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), 125, 525, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
-  cocobot_trajectory_wait();
-  cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), -1000, -500, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
-  cocobot_trajectory_wait();
-  cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), 1100, -550, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
-  cocobot_trajectory_wait();
-  _fakebot.x = -200;
-  _fakebot.y = 200;
-  _fakebot.activated = COCOBOT_OPPONENT_DETECTION_ACTIVATED;
-  cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), -200, -300, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
-  cocobot_trajectory_wait();
-  cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), -500, -200, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
-  cocobot_trajectory_wait();
+  //cocobot_pathfinder_set_start_zone_allowed();
+  //cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), 125, 525, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
+  //cocobot_trajectory_wait();
+  //cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), -1000, -500, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
+  //cocobot_trajectory_wait();
+  //cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), 1100, -550, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
+  //cocobot_trajectory_wait();
+  //_fakebot.x = -200;
+  //_fakebot.y = 200;
+  //_fakebot.activated = COCOBOT_OPPONENT_DETECTION_ACTIVATED;
+  //cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), -200, -300, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
+  //cocobot_trajectory_wait();
+  //cocobot_pathfinder_execute(cocobot_position_get_x(), cocobot_position_get_y(), -500, -200, COCOBOT_PATHFINDER_MODE_EXECUTE_TRAJ_FORWARD);
+  //cocobot_trajectory_wait();
 
   while(1)
-      ;
+      vTaskDelay(100/portTICK_PERIOD_MS);
   //cocobot_action_scheduler_start();
-}
+ }
 
 void com_handler(uint16_t pid)
 {
@@ -153,7 +209,7 @@ int main(int argc, char *argv[])
   (void)argv;
 #endif
   platform_init();
-  cocobot_com_init(MCUAL_USART1, 1, 4, com_handler);
+  cocobot_com_init(MCUAL_USART1, 1, 1, com_handler);
   cocobot_lcd_init();
   cocobot_position_init(4);
   cocobot_action_scheduler_init();
@@ -165,25 +221,20 @@ int main(int argc, char *argv[])
   cocobot_action_scheduler_use_pathfinder(1);
 
   //set initial position
-  //switch(cocobot_game_state_get_color())
-  //{
-   // case COCOBOT_GAME_STATE_COLOR_NEG:
-   //   cocobot_position_set_x(-1300);
-   //   cocobot_position_set_y(250);
-   //   cocobot_position_set_angle(180);
-   //   break;
-
-   // case COCOBOT_GAME_STATE_COLOR_POS:
-   //   cocobot_position_set_x(1300);
-   //   cocobot_position_set_y(250);
-   //   break;
-  //}
-
+  switch(cocobot_game_state_get_color())
+  {
+    case COCOBOT_GAME_STATE_COLOR_NEG:
   cocobot_position_set_x(-1300);
-  cocobot_position_set_y(500);
-  //cocobot_position_set_x(-1000);
-  //cocobot_position_set_y(-500);
-  cocobot_position_set_angle(180);
+      cocobot_position_set_y(250);
+      cocobot_position_set_angle(0);
+      break;
+
+    case COCOBOT_GAME_STATE_COLOR_POS:
+      cocobot_position_set_x(1280);
+      cocobot_position_set_y(750);
+      cocobot_position_set_angle(-90);
+      break;
+  }
       
   xTaskCreate(run_strategy, "strat", 600, NULL, 2, NULL );
   xTaskCreate(update_lcd, "blink", 200, NULL, 1, NULL );
