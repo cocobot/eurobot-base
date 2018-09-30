9b3c7b46c0e4729a94a140ae722e8692af54297e
Vincent Faure <vincent.hr.faure@gmail.com>
2018-09-30 19:02:32 +0200

    Add tests for vl53l0x

--- projects/stm32f407-disc/main.c
+++ projects/stm32f407-disc/main.c
@@ -3,18 +3,40 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <mcual.h>
-//#include <cocobot.h>
+#include <vl53l0x.h>
 
+static int toto = 0;
 
 void update_lcd(void * arg)
 {
     (void)arg;
 
+    float distance = 300;
     //int32_t zero = cocobot_position_get_distance();
     while(1)
     {
         //toggle led
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        distance = vl53l0x_get_distance_mm(toto);
+        if(distance > 300)
+        {
+            platform_led_set(PLATFORM_LED_BLUE);
+            platform_led_clear(PLATFORM_LED_GREEN);
+            platform_led_clear(PLATFORM_LED_ORANGE);
+        }
+        else if(distance < 300)
+        {
+            platform_led_set(PLATFORM_LED_GREEN);
+            platform_led_clear(PLATFORM_LED_BLUE);
+            platform_led_clear(PLATFORM_LED_ORANGE);
+        }
+        else if(distance == 300)
+        {
+            platform_led_set(PLATFORM_LED_ORANGE);
+            platform_led_clear(PLATFORM_LED_BLUE);
+            platform_led_clear(PLATFORM_LED_GREEN);
+        }
+
         platform_led_toggle(PLATFORM_LED_RED);
     }
 }
@@ -29,6 +51,9 @@ int main(int argc, char *argv[])
     (void)argv;
 #endif
     platform_init();
+    vl53l0x_init(3);
+    toto = vl53l0x_register_device(0x53);
+
 
     xTaskCreate(update_lcd, "blink", 200, NULL, 1, NULL );
 
