d426ceb511a3e06d278638b3cea126b179696e51
Damien Espitallier <damien.espitallier@gmail.com>
2018-10-26 20:49:55 +0200

    base for bootloader with libcanard

--- ausbee-packages/libcocobot/src/can/can.c
+++ ausbee-packages/libcocobot/src/can/can.c
@@ -1,8 +1,36 @@
 #include <include/generated/autoconf.h>
 #ifdef CONFIG_LIBCOCOBOT_CAN
 
+#include <canard.h>
+
+static CanardInstance _canard;
+static uint8_t _canard_memory_pool[1024];
+
+
+static void onTransferReceived(CanardInstance* ins,
+                               CanardRxTransfer* transfer)
+{
+  (void)ins;
+  (void)transfer;
+}
+
+static bool shouldAcceptTransfer(const CanardInstance* ins,
+                                 uint64_t* out_data_type_signature,
+                                 uint16_t data_type_id,
+                                 CanardTransferType transfer_type,
+                                 uint8_t source_node_id)
+{
+  (void)ins;
+  (void)out_data_type_signature;
+  (void)data_type_id;
+  (void)transfer_type;
+  (void)source_node_id;
+  return false;
+}
+
 void cocobot_can_init(void)
 {
+  canardInit(&_canard, _canard_memory_pool, sizeof(_canard_memory_pool), onTransferReceived, shouldAcceptTransfer, NULL);
 }
 
 #endif
--- projects/brain_2019_loader/main.c
+++ projects/brain_2019_loader/main.c
@@ -1,19 +1,16 @@
 #include <platform.h>
 #include <cocobot.h>
 
-static void event(void)
-{
-  platform_led_toggle(PLATFORM_LED0);
-}
+//static void event(void)
+//{
+//  platform_led_toggle(PLATFORM_LED0);
+//}
 
 int main(void) 
 {
   platform_init();
 
   cocobot_can_init();
-
-  mcual_loader_init(MCUAL_USART1, event);
-  mcual_loader_run();
 
   return 0;
 }
