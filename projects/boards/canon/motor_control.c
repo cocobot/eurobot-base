9e7df3d69d1fed2df585135dacc96931c53d9dd4
rdenieport <denieport.enseirg@gmail.com>
2019-04-28 12:29:58 +0200

    update motor_control

--- projects/boards/canon/motor_control.c
+++ projects/boards/canon/motor_control.c
@@ -1,30 +1,20 @@
 #include <platform.h>
 #include "motor_control.h"
+#include "pid.h"
 
 /**
  * @brief Frequency of pwm outputs in kilohertz
  */
 #define MOTOR_CONTROL_PWM_FREQUENCY_kHz 20000
 #define MOTOR_CONTROL_MAX_PWM 1000
-#define MOTOR_SERVO_US 100
-#define MOTOR_POLES 6
+#define MOTOR_CONTROL_PWM_FACTOR 0.1
+#define MOTOR_CONTROL_SERVO_REFRES_US 100
+#define MOTOR_CONTROL_POLES 6
 
 /*********************************
  * Global variables definition
  *********************************/
 
-/*************************************
- * HALL PHASE / PWM Sync - ClockWise
-* Normal HALL 	#		Phase sequence
- *	#		U	V	W				DRIVE	U		V		W
- * 	0		0	0	1		1					0		PWM	HZ
- * 	1		0	1	1		3					PWM	0		HZ
- * 	2		0	1	0		2					PWM HZ	0
- * 	3		1	1	0		6					0		HZ	PWM
- * 	4		1	0	0		4					HZ	0		PWM
- * 	5		1	0	1		5					HZ	PWM	0
- *************************************/
-static const unsigned int _Hall[7] = {-1, 0, 2, 1, 4, 5, 3};
 
 /*timestamp var*/
 static uint64_t _Hall_timestamp_us = 0;
@@ -33,26 +23,25 @@ static uint64_t _Servo_timestamp_us = 0;
 /*motor parameter*/
 static float _Velocity = 0;
 static int _Phase = 0;
-static volatile int64_t _Cur_PWM = 0; 
 
 struct motor_driver_pin {
- uint32_t en;
- uint32_t pwm;
+	uint32_t en;
+	uint32_t pwm;
 };
 
 static struct motor_driver_pin _Driver_pins[3]={
-		{PLATFORM_GPIO_UEN, PLATFORM_PWM_U},
-		{PLATFORM_GPIO_VEN, PLATFORM_PWM_V},
-		{PLATFORM_GPIO_WEN, PLATFORM_PWM_W}
-	};
+	{PLATFORM_GPIO_UEN, PLATFORM_PWM_U},
+	{PLATFORM_GPIO_VEN, PLATFORM_PWM_V},
+	{PLATFORM_GPIO_WEN, PLATFORM_PWM_W}
+};
 
 /********************************
  * Private functions declaration
  ********************************/
-static void _set_motor_pwm(void); //control mosfet drivers
-static int motor_control_get_phase(void); //reads HALL inputs
+static void _set_motor_pwm(int32_t pwm); //control mosfet drivers
+static int _motor_control_get_hall(void); //reads HALL inputs
 /*compute current speed and phase configuration*/
-static void motor_control_speed(int phase, uint64_t timestamp_us); 
+static int _motor_control_update_speed(int phase, uint64_t timestamp_us); 
 
 
 /* for debug only*/
@@ -68,19 +57,11 @@ void printerror(char* c){
 /**
  * @brief Return current velocity, based on Hall sensors
  */
-float get_velocity(void){
+float motor_control_get_velocity(void){
 	return _Velocity;
 }
 
-void motor_control_init(void)
-{
-#if 0
-	//set hall sensor input pins as interrupt source
-	platform_gpio_set_interrupt(PLATFORM_GPIO_UHALL, MCUAL_GPIO_BOTH_EDGE, motor_control_hall_callback);
-	platform_gpio_set_interrupt(PLATFORM_GPIO_VHALL, MCUAL_GPIO_BOTH_EDGE, motor_control_hall_callback);
-	platform_gpio_set_interrupt(PLATFORM_GPIO_WHALL, MCUAL_GPIO_BOTH_EDGE, motor_control_hall_callback);
-#endif
-
+void motor_control_init(void){
 	/*dissable all PWM Drivers*/
 	platform_gpio_clear(PLATFORM_GPIO_UEN | PLATFORM_GPIO_VEN | PLATFORM_GPIO_WEN);
 
@@ -93,67 +74,82 @@ void motor_control_init(void)
 
 
 void motor_control_process_event(uint64_t timestamp_us){
-	int phase;
+	int phase = _motor_control_get_hall();
+	float speed_val;
+
+	if (phase == -1){// error on hall
+		printerror("Invalid Hall value !\n");
+		return;
+	}
+	if (_Phase != phase){ //motor positon changed. Compute new speed
+		if (_motor_control_update_speed(phase, timestamp_us) < 0){
+			printerror("Invalid Hall value !\n");
+			return;
+		}
+	}
 
 	/*time to reevaluate servo loop*/
-	if (timestamp_us - _Servo_timestamp_us > MOTOR_SERVO_US){
-		// motor_compute_servo();
+	if (timestamp_us - _Servo_timestamp_us > MOTOR_CONTROL_SERVO_REFRES_US){
+
+		/*compute pid*/
+		speed_val = pid_update(_Velocity, timestamp_us - _Servo_timestamp_us);
+		if (pid_is_limited()){
+			printerror("Warning : Quadramp or speed limit\n");
+		}
+
+		/*transform for float to pwm (signed) and update pwm*/
+		_set_motor_pwm((int32_t)(speed_val * MOTOR_CONTROL_PWM_FACTOR));
+		_Servo_timestamp_us = timestamp_us;
 	}
 
-	/*someting changed on halls*/
-	if (_Phase != (phase = motor_control_get_phase())){ //hall phase has changed
-		motor_control_speed(phase, timestamp_us);
-	}
-
-	_set_motor_pwm();
 	return;
 }
 
 void motor_control_set_config(float kp, float ki, float imax, float max_speed_rpm)
 {
-	(void)kp;
-	(void)ki;
-	(void)imax;
-	(void)max_speed_rpm;
+	pid_set(kp, ki, 0.0);
+	pid_set_limit(max_speed_rpm,imax);
+	pid_set_cons(0.0);
+	pid_reset();
+
+	return;
 }
 
 void motor_control_set_setpoint(uint8_t enable, float rpm)
 {
-	(void)enable;
-	(void)rpm;
-}
-
-/**
- * @brief Stop motor and reset speed taget
- * */
-void motor_control_stop(void){
 	int i;
 
-	for (i = 0; i < 3; i++){
-		platform_gpio_clear(_Driver_pins->en);
-		platform_set_duty_cycle(_Driver_pins->pwm,0);
+	if (enable){
+		pid_set_cons(rpm);
+	}
+	else {
+		for (i = 0; i < 3; i++){
+			platform_gpio_clear(_Driver_pins->en);
+			platform_set_duty_cycle(_Driver_pins->pwm,0);
+		}
+
+		pid_set_cons(0.0);
+		pid_reset();
 	}
 
-	_Cur_PWM = 0;
 	return;
 }
 
 
 /**********************************
  * Private Function Definition
-***********************************/
+ ***********************************/
 /*
  * @brief Private function. Set PWM/EN to each drivers
  * See _Hall comment for drive definitions
  * Note : Not optimized for execution time AT ALL
  * Should be ok though
  */
-static void _set_motor_pwm(void){
+static void _set_motor_pwm(int32_t pwm){
 	int i;
-	uint32_t pwm;
-	int signum;
 	struct motor_driver_pin * pin;
-	static const int Motor_phase[6][3] = {
+	int32_t motor_pin_val;
+	static const int32_t motor_phases[6][3] = {
 		{ 0, 1,-1},
 		{ 1, 0,-1},
 		{ 1,-1, 0},
@@ -162,95 +158,97 @@ static void _set_motor_pwm(void){
 		{-1, 1, 0}
 	};
 
-
-	/*direction of rotation and PWM cast*/
-	if(_Cur_PWM < 0){
-		pwm = (uint32_t)( -_Cur_PWM);
-		signum = -1;
-	}
-	else{
-		pwm = (uint32_t)_Cur_PWM;
-		signum = 1;
-	}
-
 	/*set IO according to direction and current phase*/
 	for (i = 0; i < 3; i++){ //sweeping U, V, W phase
-		pin = _Driver_pins + i; //current phase pin pointers
 
-		switch(Motor_phase[_Phase][i] * signum){
-			case -1 : // Should be HZ
-				platform_gpio_clear(pin->en);
-				platform_set_duty_cycle(pin->pwm, 0);
-				break;
+		pin = &(_Driver_pins[i]); //current phase pin pointers
+		motor_pin_val = pwm * motor_phases[_Phase][i]; //current pwm value 
 
-			case 0: // Should be grounded
-				platform_gpio_set(pin->en);
-				platform_set_duty_cycle(pin->pwm, 0);
-				break;
-
-			case 1 : // Should be PWM
-				platform_gpio_set(pin->en);
-				platform_set_duty_cycle(pin->pwm, pwm);
-				break;
-
-			default: //Should not append
-				/*
-				 * @TODO : implement error catch
-				 */ 
-				break;
+		if (motor_pin_val > 0){ //should be PWM
+			platform_set_duty_cycle(pin->pwm, pwm);
+			platform_gpio_set(pin->en);
+		}
+		else if (motor_pin_val == 0){ // Should be grounded
+			platform_set_duty_cycle(pin->pwm, 0);
+			platform_gpio_set(pin->en);
+		} 
+		else { // should be HZ
+			platform_gpio_clear(pin->en);
+			platform_set_duty_cycle(pin->pwm, 0);
 		}
 	}
 	return;
 }
 
-static int motor_control_get_phase(void){
 
-	/*get current hall value*/
-	const unsigned int hall = 
+/************************************************
+ * HALL PHASE 						 PWM Sync - ClockWise
+ * Phase HALL 		Val Val-1			Phase sequence
+ *	#		U	V	W							DRIVE	U		V		W
+ * 	0		0	0	1		1		0						0		PWM	HZ
+ * 	1		0	1	1		3		2						PWM	0		HZ
+ * 	2		0	1	0		2		1						PWM HZ	0
+ * 	3		1	1	0		6		5						0		HZ	PWM
+ * 	4		1	0	0		4		3						HZ	0		PWM
+ * 	5		1	0	1		5		4						HZ	PWM	0
+ ***********************************************/
+/**
+ * @brief : convert hall sensors value to phase value
+ * @return phase number, -1 if error
+ * */
+static int _motor_control_get_hall(void){
+	static const unsigned int hall_to_phase[6] = {0, 2, 1, 4, 5, 3};
+
+	const unsigned int hall = //get current hall values
 		(!!platform_gpio_get(PLATFORM_GPIO_UHALL)<<2) |
 		(!!platform_gpio_get(PLATFORM_GPIO_VHALL)<<1) |
 		!!platform_gpio_get(PLATFORM_GPIO_VHALL);
 
-	/* get current phase number - check for errors*/
-	if ((hall >= 1) && (hall <= 5)){
-		return _Hall[hall];
+	if ((hall >= 1) && (hall <= 6)){ //hall value ok
+		return hall_to_phase[hall - 1];
 	}
-	else{
-		printerror("Invalid hall value !\n");
+	else{ //not a valid value
 		return -1;
 	}
 }
 
-static void motor_control_speed(int phase, uint64_t timestamp_us){
-	/*check for signum and overspeed*/
-	int delta_ph = phase - _Phase;
-	int signum;
+
+/**
+ * @brief : compute motor angular speed according to hall sensors
+ * @return : -1 if overspeed (dt = 0 or hall delta >=3)
+ * @return : 0 if nothing changed
+ * @return : 1 if update
+ */
+static int _motor_control_update_speed(int phase, uint64_t timestamp_us){
+
+	int delta_ph = phase - _Phase; //get phase delta.
+	uint64_t dt = timestamp_us - _Hall_timestamp_us; //get time delta;
+	float dangle; //angle delta in fraction of turn
+
+	if (dt == 0){ // somethings wrong
+		return -1;
+	}
+
+	/*correct the phase delta (rollover at 5)*/
+	if (delta_ph <= -3){
+		delta_ph += 6;
+	}
 
 	if (delta_ph == 0){ //artefact
-		return;
+		return 0;
+	}
+	if (delta_ph == 3){ //impossible to tell direction of rotation
+		return -1;
 	}
 
-	/* normal move and fringe cases*/
-	if ((delta_ph == 1) || ((phase == 0) && (_Phase == 5))){ 
-		/* clockwise*/
-		signum = 1;
-	}
-	else if ((delta_ph == -1) || ((phase == 5) && (_Phase == 0))) {
-		/* counter clockwise */
-		signum = -1;
-	}
-	else { //error
-		printerror("Overspeed !\n");
-		return;
-	}
-	_Velocity = signum * MOTOR_POLES / 6.0 / (float)(_Hall_timestamp_us - timestamp_us);	
+	/*update global variables*/
 
-	/*uptage global variables*/
-
+	dangle = (float)delta_ph  / (float)(MOTOR_CONTROL_POLES * 6); 
+	_Velocity = dangle / ((float)(dt) * 1e-6 ) * 60;
 	_Phase = phase;
 	_Hall_timestamp_us = timestamp_us;
 
-	return; 
+	return 1; 
 }
 
 
