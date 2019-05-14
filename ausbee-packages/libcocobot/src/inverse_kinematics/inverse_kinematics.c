211e62b7211dea90a1e9108774643b15d9a20a37
David Bitonneau <david.bitonneau@gmail.com>
2019-05-14 20:18:38 +0200

    Update kinematics module: forward and inverse functions

--- ausbee-packages/libcocobot/src/inverse_kinematics/inverse_kinematics.c
+++ ausbee-packages/libcocobot/src/inverse_kinematics/inverse_kinematics.c
@@ -6,6 +6,10 @@
 #include <task.h>
 #include <math.h>
 
+#define DEG2RAD(a) ((a) * M_PI / 180)
+#define RAD2DEG(a) ((a) * 180 / M_PI)
+#define SQUARE(x)  ((x) * (x))
+
 static struct cocobot_kinematics_t
 {
   float d1;
@@ -18,18 +22,18 @@ static struct cocobot_kinematics_t
 
 static struct cocobot_kinematics_limits_t
 {
-    float a1Min; float a1Max;
-    float a2Min; float a2Max;
-    float a3Min; float a3Max;
-    float a4Min; float a4Max;
+    float a1Min_deg; float a1Max_deg;
+    float a2Min_deg; float a2Max_deg;
+    float a3Min_deg; float a3Max_deg;
+    float a4Min_deg; float a4Max_deg;
 
     float xMin; float xMax;
     float yMin; float yMax;
     float zMin; float zMax;
-    float alphaMin; float alphaMax;
+    float alphaMin_deg; float alphaMax_deg;
 } cocobot_kinematics_limits;
 
-void cocobot_inverse_kinematics_init_DH_parameters(float d1, float r1, float r2, float r3, float r4, float e4)
+void cocobot_kinematics_init_DH_parameters(float d1, float r1, float r2, float r3, float r4, float e4)
 {
     // Paramètres géométriques robot
     cocobot_kinematics.d1 = d1;
@@ -37,62 +41,95 @@ void cocobot_inverse_kinematics_init_DH_parameters(float d1, float r1, float r2,
     cocobot_kinematics.r2 = r2;
     cocobot_kinematics.r3 = r3;
     cocobot_kinematics.r4 = r4;
-    cocobot_kinematics.e4 = e4;
+    cocobot_kinematics.e4 = e4; // (décallage ventouse en "y" par rapport aux axes du bras)
 
     // Positions limites articulations robot (en deg)
-    cocobot_kinematics_limits.a1Min=-180; cocobot_kinematics_limits.a1Max=180;
-    cocobot_kinematics_limits.a2Min=- 90; cocobot_kinematics_limits.a2Max= 90;
-    cocobot_kinematics_limits.a3Min=-160; cocobot_kinematics_limits.a3Max=160;
-    cocobot_kinematics_limits.a4Min=- 90; cocobot_kinematics_limits.a4Max= 90;
+    cocobot_kinematics_limits.a1Min_deg=-180; cocobot_kinematics_limits.a1Max_deg=180;
+    cocobot_kinematics_limits.a2Min_deg=- 90; cocobot_kinematics_limits.a2Max_deg= 90;
+    cocobot_kinematics_limits.a3Min_deg=-160; cocobot_kinematics_limits.a3Max_deg=160;
+    cocobot_kinematics_limits.a4Min_deg=- 90; cocobot_kinematics_limits.a4Max_deg= 90;
 
     // Positions limites commande effecteur (en m)
     cocobot_kinematics_limits.xMin= 0.000;   cocobot_kinematics_limits.xMax=0.3337;
     cocobot_kinematics_limits.yMin=-0.370;   cocobot_kinematics_limits.yMax=0.370;
     cocobot_kinematics_limits.zMin= 0.000;   cocobot_kinematics_limits.zMax=0.370;
-    cocobot_kinematics_limits.alphaMin=-180; cocobot_kinematics_limits.alphaMax=180;
+    cocobot_kinematics_limits.alphaMin_deg=-180; cocobot_kinematics_limits.alphaMax_deg=180;
 }
 
-void cocobot_inverse_kinematics_set_angle_limits(float a1Min, float a1Max, float a2Min, float a2Max, float a3Min, float a3Max, float a4Min, float a4Max)
+void cocobot_kinematics_set_angle_limits(float a1Min_deg, float a1Max_deg, float a2Min_deg, float a2Max_deg, float a3Min_deg, float a3Max_deg, float a4Min_deg, float a4Max_deg)
 {
-    cocobot_kinematics_limits.a1Min=a1Min; cocobot_kinematics_limits.a1Max=a1Max;
-    cocobot_kinematics_limits.a2Min=a2Min; cocobot_kinematics_limits.a2Max=a2Max;
-    cocobot_kinematics_limits.a3Min=a3Min; cocobot_kinematics_limits.a3Max=a3Max;
-    cocobot_kinematics_limits.a4Min=a4Min; cocobot_kinematics_limits.a4Max=a4Max;
+    cocobot_kinematics_limits.a1Min_deg=a1Min_deg; cocobot_kinematics_limits.a1Max_deg=a1Max_deg;
+    cocobot_kinematics_limits.a2Min_deg=a2Min_deg; cocobot_kinematics_limits.a2Max_deg=a2Max_deg;
+    cocobot_kinematics_limits.a3Min_deg=a3Min_deg; cocobot_kinematics_limits.a3Max_deg=a3Max_deg;
+    cocobot_kinematics_limits.a4Min_deg=a4Min_deg; cocobot_kinematics_limits.a4Max_deg=a4Max_deg;
 }
 
-void cocobot_inverse_kinematics_set_cartesian_limits(float xMin, float xMax, float yMin, float yMax, float zMin, float zMax, float alphaMin, float alphaMax)
+void cocobot_kinematics_set_cartesian_limits(float xMin, float xMax, float yMin, float yMax, float zMin, float zMax, float alphaMin_deg, float alphaMax_deg)
 {
     cocobot_kinematics_limits.xMin=xMin;         cocobot_kinematics_limits.xMax=xMax;
     cocobot_kinematics_limits.yMin=yMin;         cocobot_kinematics_limits.yMax=yMax;
     cocobot_kinematics_limits.zMin=zMin;         cocobot_kinematics_limits.zMax=zMax;
-    cocobot_kinematics_limits.alphaMin=alphaMin; cocobot_kinematics_limits.alphaMax=alphaMax;
+    cocobot_kinematics_limits.alphaMin_deg=alphaMin_deg; cocobot_kinematics_limits.alphaMax_deg=alphaMax_deg;
 }
 
-void cocobot_inverse_kinematics_compute_forward_kinematics(const cocobot_joint_pos_t * current_joint_pos, cocobot_cartesian_value_t * new_cartesian_value)
+void cocobot_kinematics_compute_forward(const cocobot_joint_pos_t * current_joint_pos, cocobot_cartesian_pos_t * new_cartesian_pos)
 {
-    float a23 = current_joint_pos->a2 + current_joint_pos->a3;
+    float a23_deg = current_joint_pos->a2_deg + current_joint_pos->a3_deg;
 
-    new_cartesian_value->alpha = current_joint_pos->a2 + current_joint_pos->a3 + current_joint_pos->a4;
+    new_cartesian_pos->alpha_deg = current_joint_pos->a2_deg + current_joint_pos->a3_deg + current_joint_pos->a4_deg;
 
 
-    float C1 = cocobot_kinematics.r2 * cosf(current_joint_pos->a2)
-        + cocobot_kinematics.r3 * cosf(a23)
-        + cocobot_kinematics.r4 * cosf(new_cartesian_value->alpha);
+    float C1 = cocobot_kinematics.r2 * cosf(DEG2RAD(current_joint_pos->a2_deg))
+             + cocobot_kinematics.r3 * cosf(DEG2RAD(a23_deg))
+             + cocobot_kinematics.r4 * cosf(DEG2RAD(new_cartesian_pos->alpha_deg));
 
-    new_cartesian_value->x = cosf(current_joint_pos->a1) * (cocobot_kinematics.r1 + C1)
-                           - sinf(current_joint_pos->a1) * cocobot_kinematics.e4;
+    new_cartesian_pos->x = cosf(DEG2RAD(current_joint_pos->a1_deg)) * (cocobot_kinematics.r1 + C1)
+                         - sinf(DEG2RAD(current_joint_pos->a1_deg)) * cocobot_kinematics.e4;
 
-    new_cartesian_value->y = sinf(current_joint_pos->a1) * (cocobot_kinematics.r1 + C1)
-                           + cosf(current_joint_pos->a1) * cocobot_kinematics.e4;
+    new_cartesian_pos->y = sinf(DEG2RAD(current_joint_pos->a1_deg)) * (cocobot_kinematics.r1 + C1)
+                         + cosf(DEG2RAD(current_joint_pos->a1_deg)) * cocobot_kinematics.e4;
 
-    new_cartesian_value->z = cocobot_kinematics.d1
-                           - cocobot_kinematics.r2 * sinf(current_joint_pos->a2)
-                           - cocobot_kinematics.r3 * sinf(a23)
-                           - cocobot_kinematics.r4 * sinf(new_cartesian_value->alpha);
+    new_cartesian_pos->z = cocobot_kinematics.d1
+                         - cocobot_kinematics.r2 * sinf(DEG2RAD(current_joint_pos->a2_deg))
+                         - cocobot_kinematics.r3 * sinf(DEG2RAD(a23_deg))
+                         - cocobot_kinematics.r4 * sinf(DEG2RAD(new_cartesian_pos->alpha_deg));
 }
 
-//void cocobot_inverse_kinematics_compute(const cocobot_cartesian_value_t * current_cartesian_value, cocobot_joint_pos_t * new_joint_pos)
-//{
-//}
+void cocobot_kinematics_compute_inverse(const cocobot_cartesian_pos_t * current_cartesian_pos, cocobot_joint_pos_t * new_joint_pos)
+{
+    float r_target = sqrtf(SQUARE(current_cartesian_pos->x) + SQUARE(current_cartesian_pos->y));
+
+    new_joint_pos->a1_deg = RAD2DEG(atan2f(current_cartesian_pos->y, current_cartesian_pos->x)
+                                  - atan2f(cocobot_kinematics.e4,r_target));
+
+    new_joint_pos->a3_deg = RAD2DEG(acosf(
+                (SQUARE(r_target - cocobot_kinematics.r1
+                      - cocobot_kinematics.r4 * cosf(DEG2RAD(current_cartesian_pos->alpha_deg)))
+               + SQUARE(cocobot_kinematics.d1 - current_cartesian_pos->z
+                      - cocobot_kinematics.r4 * sinf(DEG2RAD(current_cartesian_pos->alpha_deg)))
+               - SQUARE(cocobot_kinematics.r2)
+               - SQUARE(cocobot_kinematics.r3))
+              / (2*cocobot_kinematics.r2*cocobot_kinematics.r3)));
+
+    new_joint_pos->a2_deg = RAD2DEG(
+            atan2f((cocobot_kinematics.d1 - current_cartesian_pos->z
+                  - cocobot_kinematics.r4 * sinf(DEG2RAD(current_cartesian_pos->alpha_deg))),
+                   (r_target - cocobot_kinematics.r1
+                  - cocobot_kinematics.r4 * cosf(DEG2RAD(current_cartesian_pos->alpha_deg))))
+          - atan2f((cocobot_kinematics.r3 * sinf(DEG2RAD(new_joint_pos->a3_deg))),
+                   (cocobot_kinematics.r2 + cocobot_kinematics.r3*cosf(DEG2RAD(new_joint_pos->a3_deg)))));
+
+    new_joint_pos->a4_deg = current_cartesian_pos->alpha_deg - new_joint_pos->a2_deg - new_joint_pos->a3_deg;
+}
+
+void cocobot_kinematics_print_joint_pos(const cocobot_joint_pos_t * joint_pos)
+{
+    printf("a1_deg=%.3f, a2_deg=%.3f, a3_deg=%.3f, a4_deg=%.3f\n", joint_pos->a1_deg, joint_pos->a2_deg, joint_pos->a3_deg, joint_pos->a4_deg);
+}
+
+void cocobot_kinematics_print_cartesian_pos(const cocobot_cartesian_pos_t * cartesian_pos)
+{
+    printf("x=%.3f, y=%.3f, z=%.3f, alpha_deg=%.3f\n", cartesian_pos->x, cartesian_pos->y, cartesian_pos->z, cartesian_pos->alpha_deg);
+}
 
 #endif
--- projects/boards/mecanele/main.c
+++ projects/boards/mecanele/main.c
@@ -11,7 +11,59 @@ int main(void)
   cocobot_com_init();
   cocobot_com_run();
 
-  cocobot_inverse_kinematics_init_DH_parameters(0.0971, 0.0637, 0.150, 0.080, 0.040, -0.0117);
+  cocobot_kinematics_init_DH_parameters(0.0971, 0.0637, 0.150, 0.080, 0.040, -0.0117);
+
+  cocobot_kinematics_set_angle_limits(-180, 180,
+                                              - 90,  90,
+                                              -160, 160,
+                                              - 90,  90);
+
+  cocobot_kinematics_set_cartesian_limits(0.000, 0.3337,
+                                                 -0.370, 0.37,
+                                                  0.000, 0.37,
+                                                   -180,  180);
+
+  cocobot_joint_pos_t joint_pos = { .a1_deg = 0, .a2_deg = 0, .a3_deg = 0, .a4_deg = 0 };
+  cocobot_cartesian_pos_t cartesian_pos;
+
+  cocobot_kinematics_compute_forward(&joint_pos, &cartesian_pos);
+
+  printf("-------------------------------\n");
+  printf("Joint pos :\n");
+  cocobot_kinematics_print_joint_pos(&joint_pos);
+
+  printf("Cartesian pos :\n");
+  cocobot_kinematics_print_cartesian_pos(&cartesian_pos);
+
+  joint_pos.a1_deg = -15;
+  joint_pos.a2_deg = -30;
+  joint_pos.a3_deg = 75;
+  joint_pos.a4_deg = -10;
+
+  cocobot_kinematics_compute_forward(&joint_pos, &cartesian_pos);
+
+  printf("-------------------------------\n");
+  printf("Joint pos :\n");
+  cocobot_kinematics_print_joint_pos(&joint_pos);
+
+  printf("Cartesian pos :\n");
+  cocobot_kinematics_print_cartesian_pos(&cartesian_pos);
+
+  cartesian_pos.x = 0.23;
+  cartesian_pos.y = -0.023;
+  cartesian_pos.z = 0.13;
+  cartesian_pos.alpha_deg = -42;
+
+  cocobot_kinematics_compute_inverse(&cartesian_pos, &joint_pos);
+
+  printf("-------------------------------\n");
+  printf("Cartesian pos :\n");
+  cocobot_kinematics_print_cartesian_pos(&cartesian_pos);
+
+  printf("Joint pos :\n");
+  cocobot_kinematics_print_joint_pos(&joint_pos);
+  printf("-------------------------------\n");
+
 
   vTaskStartScheduler();
 
