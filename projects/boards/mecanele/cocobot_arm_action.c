bd6d14c60ece42fe91a50b545fb9050f30066528
David Bitonneau <david.bitonneau@gmail.com>
2019-05-21 19:10:24 +0200

    Module arm_action prepare function before angles tuning

--- projects/boards/mecanele/cocobot_arm_action.c
+++ projects/boards/mecanele/cocobot_arm_action.c
@@ -29,3 +29,133 @@ void cocobot_arm_action_init(void)
   cocobot_arm_init(&arm[3], 270, 0, 0, 0);
   cocobot_arm_print_pos(&arm[3]);
 }
+
+void cocobot_arm_action_prendre_palais_sol(int arm_id, float x, float y)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_cartesian(arm[arm_id], x, y, z, alpha_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_pos_stockage_palais(int arm_id)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_prise_distributeur(int arm_id, float angle)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_orientation(int arm_id, int side)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_prise_goldenium(int arm_id, float angle)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_repos_goldenium(int arm_id)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_prise_bluenium(int arm_id, float angle)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_depose_balance(int arm_id, float angle)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_depose_accelerateur_particules(int arm_id, float angle, float distance)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
+
+void cocobot_arm_action_depose_case(int arm_id, float angle)
+{
+  if (arm_id >= 0 && arm_id <= 3)
+  {
+    // TODO: à régler !
+    //cocobot_arm_move_arti(arm[arm_id], a1_deg, a2_deg, a3_deg, a4_deg);
+  }
+  else
+  {
+    printf("Wrong arm_id: %d should be between 0 and 3.\n", arm_id);
+  }
+}
--- projects/boards/mecanele/cocobot_arm_action.h
+++ projects/boards/mecanele/cocobot_arm_action.h
@@ -1,6 +1,18 @@
 #ifndef COCOBOT_ARM_ACTION_H
 #define COCOBOT_ARM_ACTION_H
 
+// Function to call before any other function in this module
 void cocobot_arm_action_init(void);
+// arm_id value should be between 0 and 3
+void cocobot_arm_action_prendre_palais_sol(int arm_id, float x, float y);
+void cocobot_arm_action_pos_stockage_palais(int arm_id);
+void cocobot_arm_action_prise_distributeur(int arm_id, float angle);
+void cocobot_arm_action_orientation(int arm_id, int side);
+void cocobot_arm_action_prise_goldenium(int arm_id, float angle);
+void cocobot_arm_action_repos_goldenium(int arm_id);
+void cocobot_arm_action_prise_bluenium(int arm_id, float angle);
+void cocobot_arm_action_depose_balance(int arm_id, float angle);
+void cocobot_arm_action_depose_accelerateur_particules(int arm_id, float angle, float distance);
+void cocobot_arm_action_depose_case(int arm_id, float angle);
 
 #endif /* COCOBOT_ARM_ACTION_H */
