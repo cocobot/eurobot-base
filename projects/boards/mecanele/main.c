7cce2d0d123d7ce455010bf1d73cfb372a3105da
Vincent Faure <vincent.hr.faure@gmail.com>
2019-05-26 16:27:43 +0200

    Fix i2c address and tests for all servos

--- projects/boards/mecanele/main.c
+++ projects/boards/mecanele/main.c
@@ -4,8 +4,9 @@
 #include <cocobot.h>
 #include <stdio.h>
 #include "cocobot_arm_action.h"
+#include "pcm9685.h"
 
-void blink(void)
+static void blink(void * arg)
 {
     while(1)
     {
@@ -14,16 +15,43 @@ void blink(void)
     }
 }
 
+static void servo(void * arg)
+{
+    static uint32_t value = 100;
+    while(1)
+    {
+        platform_servo_set_value(PLATFORM_SERVO_0_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_1_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_2_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_3_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_4_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_5_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_6_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_7_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_8_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_9_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_10_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_11_ID, value);
+        platform_servo_set_value(PLATFORM_SERVO_12_ID, value);
+        value += 10;
+        if(value == 500)
+            value = 100;
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    } 
+}
+
 
 int main(void) 
 {
   platform_init();
 
-  cocobot_com_init();
-  cocobot_com_run();
+  //cocobot_com_init();
+  //cocobot_com_run();
 
-  cocobot_arm_action_init();
+  //cocobot_arm_action_init();
 
+  xTaskCreate(blink, "can", 1024, NULL, 1, NULL);
+  xTaskCreate(servo, "servo", 1024, NULL, 1, NULL);
   vTaskStartScheduler();
 
   return 0;
